6f12c93b33365a004e46b4cfd90213d36398c1e3
zmc576700954 <46336096+zmc576700954@users.noreply.github.com>
2025-05-23 14:45:38 +0800

    修改一些配置

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->validateCsrfTokens(except: [
+            'wechat',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
